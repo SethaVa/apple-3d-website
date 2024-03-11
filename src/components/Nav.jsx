1dbe848ff9c54d3e9a3dbfeccad584538cf2a74c
Setha Va <vasetha1@gmail.com>
2024-03-11 13:29:14 +0700

    navigation bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,6 @@
 import logo from "../assets/images/logo.svg";
+import Search from "../assets/images/search.svg";
+import Store from "../assets/images/store.svg";
 
 const Nav = () => {
   return (
@@ -7,6 +9,42 @@ const Nav = () => {
         <ul className=" list-styled">
           <li>
             <img src={logo} alt="Apple" />
+          </li>
+          <li>
+            <a className=" link-styled">Store</a>
+          </li>
+          <li>
+            <a className=" link-styled">Mac</a>
+          </li>
+          <li>
+            <a className=" link-styled">Ipad</a>
+          </li>
+          <li>
+            <a className=" link-styled">Iphone</a>
+          </li>
+          <li>
+            <a className=" link-styled">Watch</a>
+          </li>
+          <li>
+            <a className=" link-styled">AirPods</a>
+          </li>
+          <li>
+            <a className=" link-styled">Tv & Home</a>
+          </li>
+          <li>
+            <a className=" link-styled">Entertainment</a>
+          </li>
+          <li>
+            <a className=" link-styled">Accessories</a>
+          </li>
+          <li>
+            <a className=" link-styled">Support</a>
+          </li>
+          <li>
+            <img src={Search} className=" link-styled" alt="search" />
+          </li>
+          <li>
+            <img src={Store} className=" link-styled" alt="store" />
           </li>
         </ul>
       </div>
